4a01c1bb63ffd6ec685683e78ca6ea1de10f104b
Gulshan Kumar <91527759+gulshancse24@users.noreply.github.com>
2024-04-02 08:36:38 +0530

    Update IAS.cmd

--- IAS.cmd
+++ IAS.cmd
@@ -370,7 +370,7 @@ if not defined terminal mode 75, 28
 
 echo:
 echo:
-call :_color2 %_White% "             " %_Green% "Create By Piash"
+call :_color2 %_White% "             " %_Green% "Create By Guddu_Pandit"
 echo:            ___________________________________________________ 
 echo:
 echo:               Telegram: @tg_gprajapati
@@ -392,7 +392,7 @@ choice /C:123450 /N
 set _erl=%errorlevel%
 
 if %_erl%==6 exit /b
-if %_erl%==5 start https://github.com/gulshancse24/IDM-Activation-Script & goto MainMenu
+if %_erl%==5 start https://github.com/lstprjct/IDM-Activation-Script & goto MainMenu
 if %_erl%==4 start https://www.internetdownloadmanager.com/download.html & goto MainMenu
 if %_erl%==3 goto _reset
 if %_erl%==2 (set frz=1&goto :_activate)
